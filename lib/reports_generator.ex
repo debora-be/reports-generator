95c916313e98363c5b325fa73579fdfafe37873f
somosanimais <arobedbee@gmail.com>
2022-05-15 18:18:03 -0300

    creating function handle_file

---
+++ lib/reports_generator.ex
@@ -0,0 +1,16 @@
+defmodule ReportsGenerator do
+  @moduledoc """
+  Documentation for `ReportsGenerator`.
+  """
+
+  @doc """
+  """
+  def build(filename) do
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, file_content}), do: file_content
+  defp handle_file({:error, _reason}), do: "Error while opening file"
+end
