ac63d2eb40573cf2c66b920990490b15f5ea3984
somosanimais <arobedbee@gmail.com>
2022-05-16 16:09:35 -0300

    doc and function for higher_cost

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -14,6 +14,13 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  @doc """
+  Returns the highest values from a list. Exemple of a call:
+  iex(2)> "report_complete.csv" |> ReportsGenerator.build() |> ReportsGenerator.fetch_higher_cost()
+  {"13", 282953}
+  """
+  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
   defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
