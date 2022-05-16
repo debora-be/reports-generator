8cbebebd5a2161964ef4ebfe00f15d241fe6a4bd
somosanimais <arobedbee@gmail.com>
2022-05-16 14:55:57 -0300

    function 'build'

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -3,14 +3,19 @@ defmodule ReportsGenerator do
   Documentation for `ReportsGenerator`.
   """
 
+  alias ReportsGenerator.Parser
+
   @doc """
+  Given the file path, function 'build' parses it to return the amount spent for each client (report_acc)
   """
   def build(filename) do
-    "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn [id, _food_name, price], report ->
+    Map.put(report, id, report[id] + price)
+    end)
   end
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while opening file"
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
 end
