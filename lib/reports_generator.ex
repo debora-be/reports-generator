1c86f3c89224e3b4d3f67f84bff722e998396133
somosanimais <arobedbee@gmail.com>
2022-05-16 15:32:19 -0300

    doc for module and functions

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,20 +1,20 @@
 defmodule ReportsGenerator do
   @moduledoc """
-  Documentation for `ReportsGenerator`.
+  Builds the file captured in the Parser, runs it line by line and returns the amount spent for each index from 1..30
   """
 
   alias ReportsGenerator.Parser
 
   @doc """
-  Given the file path, function 'build' parses it to return the amount spent for each client (report_acc)
+  Given the file path, build() parses it to return the amount spent (sum_values()) for each client with report_acc()
   """
   def build(filename) do
     filename
     |> Parser.parse_file()
-    |> Enum.reduce(report_acc(), fn [id, _food_name, price], report ->
-    Map.put(report, id, report[id] + price)
-    end)
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
+
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
